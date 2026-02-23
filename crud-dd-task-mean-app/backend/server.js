96f5a267f696df25ccd4f3f686a77cef07f9082b
sripad29-12 <sripad.metpelly@gmail.com>
2026-02-24 00:17:27 +0530

    Added docker files for the frontend as well as backend and a docker-compose file in the folder

--- crud-dd-task-mean-app/backend/server.js
+++ crud-dd-task-mean-app/backend/server.js
@@ -32,6 +32,6 @@ require("./app/routes/turorial.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}.`);
 });
